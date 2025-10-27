15adbf73cc0546551bb4657e3b26a4a0f694c2c6
Nithish Muddappa <43718383+nithish16-302@users.noreply.github.com>
2025-10-28 00:11:22 +0530

    Update environment.prod.ts

--- frontend-app/src/environments/environment.prod.ts
+++ frontend-app/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBase: 'https://retail-analytics-backend.railway.app' // Will be updated with actual Railway URL
+  apiBase: 'https://sentria.onrender.com' // Will be updated with actual Railway URL
 };
